e0c8887e37cd37eab411fa1e97ffadfa8b4ecbfd
elhariri noura <nouraelhariri09@gmail.com>
2020-03-28 23:48:08 +0000

    inscription etudiant fait

--- Projet1_ASP/Projet1_ASP/Controllers/EtudiantController.cs
+++ Projet1_ASP/Projet1_ASP/Controllers/EtudiantController.cs
@@ -2,8 +2,10 @@
 using Projet1_ASP.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace Projet1_ASP.Controllers
@@ -17,66 +19,117 @@ namespace Projet1_ASP.Controllers
 
         public ActionResult inscription()
         {
+            ViewBag.niv = "";
 
             ViewBag.fil = new SelectList(context.filieres, "Id_filiere", "Nom_filiere");
-          ViewBag.niv = new SelectList(context.niveaux, "id_Niveau", "nom_Niveau");
-           // ViewBag.cycle = new SelectList(context.niveaus, "id_Cycle", "nom_Cycle");
+          ViewBag.cycle = new SelectList(context.cycles, "id_Cycle", "nom_Cycle");
+       
             return View();
         }
 
 
+        public JsonResult Js(int h)
+        {
+         
+            context.Configuration.ProxyCreationEnabled = false;
+
+            return Json(context.niveaux.Where(x => x.code_cyc == h), JsonRequestBehavior.AllowGet);
+
+        }
+
 
         [HttpPost]
-        public ActionResult inscription(String b)
+        public ActionResult inscription(Etudiant e, HttpPostedFileBase file)
         {
             
-            Etudiant e = new Etudiant();
-
-            e.cne =Convert.ToInt32( Request.Form["cne"]);
-            DateTime dateTime = DateTime.Parse(Request.Form["date"]);
             
-            e.email = Request.Form["email"];
-            e.prenom = Request.Form["prenom"];
-            e.nom = Request.Form["nom"];
-            e.password = Request.Form["passe"];
-            e.date_naiss = Request.Form["date"];
-           
-            e.id_fil = Convert.ToInt32( Request.Form["filiere"]);
-            e.id_niv = Convert.ToInt32(Request.Form["niveau"]);
-            var x = context.etudiants.SingleOrDefault(p => p.email.Equals(e.email));
-            if (x != null)
+          /*  e.id_fil = Convert.ToInt32( Request.Form["filiere"]);
+            e.id_cyc = Convert.ToInt32(Request.Form["cycle"]);
+           string nomniveau = Request.Form["niveau"];
+            var x = context.niveaux.SingleOrDefault(p => p.nom_Niveau.Equals(nomniveau));
+             e.id_niv =x.id_Niveau;
+             */
+
+            if (ModelState.IsValid == false)
             {
-                return View("erreur");
+                ViewBag.fil = new SelectList(context.filieres, "Id_filiere", "Nom_filiere");
+                ViewBag.cycle = new SelectList(context.cycles, "id_Cycle", "nom_Cycle");
+                ViewBag.niv = Request.Form["niveau"];
+                return View("inscription");
             }
-            else { 
+
+
+            if (file != null && file.ContentLength > 0)
+                try
+                {
+                    string path = Path.Combine(Server.MapPath("~/Images"),Path.GetFileName(file.FileName));
+                    file.SaveAs(path);
+                    ////// base de données
+                    var length = file.InputStream.Length; //Length: 103050706
+                    MemoryStream target = new MemoryStream();
+                    file.InputStream.CopyTo(target); // generates problem in this line
+                    byte[] data = target.ToArray();
+                    e.photo = data;
+                   
+
+                }
+                catch (Exception ex)
+                {
+                   ViewBag.Message = "ERROR:" + ex.Message.ToString();
+                    return View("erreur");
+                }
+
             context.etudiants.Add(e);
+            
             context.SaveChanges();
 
-           
-
-           //Session["etudiant"] = e;
             return RedirectToAction("index","Home",e);
-            }
+            
         }
 
 
         public ActionResult espaceetudiant() {
             ViewBag.fil = new SelectList(context.filieres, "Id_filiere", "Nom_filiere");
-            ViewBag.niv = new SelectList(context.niveaux, "id_Niveau", "nom_Niveau");
+            ViewBag.niv = new SelectList(context.cycles, "id_Cycle", "nom_Cycle");
             return View();
         }
+
+
+
+
+
+
 
         public ActionResult connexion()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult connexion(Etudiant a)
+        {
+            Etudiant x = context.etudiants.SingleOrDefault(p => p.email.Equals(a.email) && p.password.Equals(a.password));
+
+            if (x != null)
+            {
+
+                Etudiant b = new Etudiant();
+                b = x;
+
+                //return View("espaceetudiant", "etudiant", x);
+                return RedirectToAction("espaceetudiant", "etudiant", b);
+            }
+            return View();
+        }
         public ActionResult erreur()
         {
             return View();
         }
 
-
+        public ActionResult test()
+        {
+            return View();
+        }
 
 
 
