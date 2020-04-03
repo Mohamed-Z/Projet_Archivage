9d8a6f26b4888777bfce83363cd0308d373d5a58
Soufiane Elhagani <soufianeelhaganie@gmail.com>
2020-04-03 19:22:37 +0100

    intégration du projet des test unitaire

---
+++ Projet1_ASP/ProjectTests/ControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Projet1_ASP;
+using Projet1_ASP.Controllers;
+
+
+namespace ProjectTests
+{
+    [TestClass]
+    public class ControllerTests
+    {
+        [TestMethod]
+        public void Index()
+        {
+            //Arrange
+            HomeController controller = new HomeController();
+
+
+            //Act
+            ViewResult result = controller.Index() as ViewResult;
+
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ Projet1_ASP/ProjectTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProjectTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProjectTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("68594e57-73c0-4f1b-b944-f278e382fe92")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
